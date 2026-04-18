89c546384455a4c9b7ae305409838765099eb85c
adiccc <adia5@post.bgu.ac.il>
2026-04-18 19:10:01 +0300

    ci and maven init

--- src/main/java/domain/event/Order.java
+++ src/main/java/domain/event/Order.java
@@ -1,8 +1,12 @@
+package domain.event;
+
+import java.util.List;
+
 public class Order {
     private int orderId;
     private int userId;
     private int eventId;
-    private List<int> tickets;
+    private List<Integer> tickets;
 
     public Order() {
         //TODO
---
+++ src/test/java/domain/event/OrderTest.java
@@ -0,0 +1,15 @@
+package domain.event;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+
+    // for ci work flow example
+    @Test
+    void example() {
+        assertTrue(true);
+    }
+
+}
