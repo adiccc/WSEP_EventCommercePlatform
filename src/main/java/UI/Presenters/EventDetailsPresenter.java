b2aee63276d1a123ccfa018d2fc4830688073d8c
ofirv <ofirvak@post.bgu.ac.il>
2026-05-29 22:59:30 +0300

    244: add lottery registration presenter

--- src/main/java/UI/Presenters/EventDetailsPresenter.java
+++ src/main/java/UI/Presenters/EventDetailsPresenter.java
@@ -1,24 +1,41 @@
 package UI.Presenters;
 
 import application.EventService;
+import application.LotteryService;
 import application.Response;
 import domain.dto.EventDetailsDTO;
 
 public class EventDetailsPresenter {
 
     private final EventService eventService;
+    private final LotteryService lotteryService;
 
-    public EventDetailsPresenter(EventService eventService) {
+    public EventDetailsPresenter(
+            EventService eventService,
+            LotteryService lotteryService
+    ) {
         this.eventService = eventService;
+        this.lotteryService = lotteryService;
     }
 
     public Response<EventDetailsDTO> getDetails(
             String token,
             int companyId,
-            int eventId) {
+            int eventId
+    ) {
         return eventService.ViewEventDetails(
                 token,
                 companyId,
+                eventId
+        );
+    }
+
+    public Response<Boolean> registerUserToLottery(
+            String token,
+            int eventId
+    ) {
+        return lotteryService.registerUserToLottery(
+                token,
                 eventId
         );
     }
