464aba475ad1bd540c23f9cd8dcf3b8ad41ff727
Lihi-Bronshtein <lihibr@post.bgu.ac.il>
2026-06-07 16:07:36 +0300

    elementPosition orm annotation

--- src/main/java/domain/dataType/ElementPosition.java
+++ src/main/java/domain/dataType/ElementPosition.java
@@ -1,11 +1,18 @@
 package domain.dataType;
 
+import jakarta.persistence.Embeddable;
+
+@Embeddable
 public class ElementPosition {
     private double x;
     private double y;
     public ElementPosition(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    protected ElementPosition() {
+        // JPA
     }
     public ElementPosition(ElementPosition elementPosition) {
         this.x = elementPosition.x;
