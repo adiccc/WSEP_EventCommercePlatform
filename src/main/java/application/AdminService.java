b44d853a97e0894ee8387e43ea05189dd41281ca
adiccc <adia5@post.bgu.ac.il>
2026-06-25 14:48:28 +0300

    check that the requesed user to suspened is not admin

--- src/main/java/application/AdminService.java
+++ src/main/java/application/AdminService.java
@@ -80,6 +80,10 @@ public class AdminService {
                         logger.log(Level.INFO, "SuspendUser failed : admin cannot suspend himself");
                         return new Response<>(false, "SuspendUser failed : admin cannot suspend himself");
                     }
+                    if(auth.isUserEmailAdmin(token,userRepo.getUserEmail(userId)).getValue()){
+                        logger.log(Level.SEVERE,"SuspendUser failed : admin cannot suspend other admin");
+                        return new Response<>(false,"SuspendUser failed : admin cannot suspend other admin");
+                    }
                     Member member=userRepo.findById(userId);
                     if(member.isSuspended()){
                         logger.log(Level.INFO, "SuspendUser failed : user is already suspended");
@@ -123,6 +127,10 @@ public class AdminService {
                     if(getUserIdFromToken(token) == userId){
                         logger.log(Level.INFO, "SuspendUser failed : admin cannot suspend himself");
                         return new Response<>(false, "SuspendUser failed : admin cannot suspend himself");
+                    }
+                    if(auth.isUserEmailAdmin(token,userRepo.getUserEmail(userId)).getValue()){
+                        logger.log(Level.SEVERE,"SuspendUser failed : admin cannot suspend other admin");
+                        return new Response<>(false,"SuspendUser failed : admin cannot suspend other admin");
                     }
                     Member member=userRepo.findById(userId);
                     if(member.isSuspended()){
