8ab2052e667029a3598d7d5478444bc557026eae
adiccc <adia5@post.bgu.ac.il>
2026-06-25 16:48:42 +0300

    fix test mock to match the new need

--- src/main/java/application/AdminService.java
+++ src/main/java/application/AdminService.java
@@ -80,11 +80,11 @@ public class AdminService {
                         logger.log(Level.INFO, "SuspendUser failed : admin cannot suspend himself");
                         return new Response<>(false, "SuspendUser failed : admin cannot suspend himself");
                     }
+                    Member member=userRepo.findById(userId);
                     if(auth.isUserEmailAdmin(token,userRepo.getUserEmail(userId)).getValue()){
                         logger.log(Level.SEVERE,"SuspendUser failed : admin cannot suspend other admin");
                         return new Response<>(false,"SuspendUser failed : admin cannot suspend other admin");
                     }
-                    Member member=userRepo.findById(userId);
                     if(member.isSuspended()){
                         logger.log(Level.INFO, "SuspendUser failed : user is already suspended");
                         return new Response<>(false, "SuspendUser failed : user is already suspended");
